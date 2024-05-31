73814bfe7d584d8740a6bdaeff5c404bc195ad9a
pedrodellolio <pedrohdellolio@gmail.com>
2024-05-30 21:37:53 -0300

    Removing classes from project

--- player.py
+++
@@ -1,8 +0,0 @@
-class Player:
-    def __init__(self, conn, port):
-        self.conn = conn
-        self.port = port
-        # self.matrix = []
-        # self.is_leader = False
-        # self.ships_sank = 0
-        # self.has_lost = False
